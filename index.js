be2e6892c3453b7f417df2922b57bc47d18fb018
Muneer Puri <muneer.p@tunicalabsmedia.com>
2022-03-14 08:42:20 +0530

    added cors check

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ const conversationRoute = require("./routes/conversations");
 const messageRoute = require("./routes/messages");
 const router = express.Router();
 const path = require("path");
-dotenv.config();
+const cors = require("cors");
+
+app.use(cors());
 app.use((req,res,next)=>{
   res.header("Access-Control-Allow-Origin","*")
   res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept, Authorization")
@@ -22,6 +24,7 @@ app.use((req,res,next)=>{
   }
   next()
 })
+dotenv.config();
 mongoose.connect(
   process.env.MONGO_URL,
   { useNewUrlParser: true, useUnifiedTopology: true },
